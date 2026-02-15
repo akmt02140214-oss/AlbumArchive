e1822523ea05a96c572fe0a981b3940691041f53
akmt02140214-oss <akmt02140214@gmail.com>
2026-02-15 10:31:19 +0900

    Initial commit: AlbumArchive project started

---
+++ src/test/java/com/albumarchive/AlbumArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albumarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
