84e42611d323d5f6404358c30f5e3b4b833fa458
akmt02140214-oss <akmt02140214@gmail.com>
2026-02-21 23:08:18 +0900

    Add処理へのコメント挿入

--- src/main/java/com/albumarchive/repository/AddAlbumRepositoryImpl.java
+++ src/main/java/com/albumarchive/repository/AddAlbumRepositoryImpl.java
@@ -38,12 +38,14 @@ public class AddAlbumRepositoryImpl implements AddAlbumRepository {
             .queryParam("format", "json")
             .toUriString();
 
+            //Last.fm APIはたくさんのデータを取得してくるため、一度そのデータをDTOで受ける
             //Getリクエスト送信とDTOへのマッピング
             LastFmResponse response = restClient.get()
                 .uri(url)
                 .retrieve()
                 .body(LastFmResponse.class);
-        //アルバム情報リスト取得        
+
+        //APIからのデータを全て受け取るDTOからAlbumDtoで使用するデータだけを受ける
         List<AlbumDto> dtoList = response.getTopalbums().getAlbum();
 
         //DTOからEntityに詰め替え
@@ -54,7 +56,8 @@ public class AddAlbumRepositoryImpl implements AddAlbumRepository {
             album.setAlbumName(dto.getAlbumName());
             album.setArtistName(dto.getArtistName());
             
-            //largeサイズの画像取得
+            //Last.fm APIは複数のアルバムジャケットサイズを返すため、解像度を選別する
+            //extralargeサイズ(300×300)を解像度に選択
             if (dto.getImage() != null && !dto.getImage().isEmpty()) {
                 for (AlbumDto.ImageDto imageDto : dto.getImage()) {
                     if ("extralarge".equals(imageDto.getSize())) {
